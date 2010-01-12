2b4ffc5c1cd935d1c4c1edd0aaf518e096a1a369
Sebastián Gamboa (SagMor) <me@sagmor.com>
2010-01-12 01:30:56 -0300

    Cleanup and error fixes

--- templates/base.rb
+++ templates/base.rb
@@ -1,4 +1,16 @@
-GOODIES_LOCATION = ENV['GOODIES_LOCATION'] || 'http://github.com/sagmor/rails-goodies/raw/master/'
+GOODIES_LOCATION = ENV['GOODIES_LOCATION'] || 'http://github.com/sagmor/rails-goodies/raw/master'
+
+
+# Some usefull methods
+def download(files)
+  files.each do |from, to|
+    run "curl -# #{GOODIES_LOCATION}/#{from} -o #{to}"
+  end
+end
+
+def patch(file, the_patch)
+  run "patch #{file} <<EOP\n#{the_patch}\nEOP"
+end
 
 # Remove unused files
 run "echo FILL ME > README"
@@ -10,20 +22,23 @@ gem 'haml'
 gem 'BlueCloth', :lib => 'bluecloth'
 gem 'mislav-will_paginate', :lib => 'will_paginate', 
                             :source   => 'http://gems.github.com'
-
+rake 'gem:install', :sudo => true
 
 # Setup git
 git :init
-run "curl #{GOODIES_LOCATION}/templates/files/gitignore -o .gitignore"
-git :add => '.', :commit => 'Initial Commit'
+download 'templates/files/gitignore' => '.gitignore'
+run "cp config/database.yml config/database.yml.sample"
+git :add => '.'
+git :commit => "-m 'Initial Commit'"
 
 
 # Install config loader
-run "curl #{GOODIES_LOCATION}/config_loader/config.yml.sample -o config/config.yml.sample"
+download  'config_loader/config.yml.sample' => 'config/config.yml.sample',
+          'config_loader/config_loader.rb' => 'lib/config_loader.rb'
+
 run "echo 'config/config.yml' >> .gitignore"
-run "curl #{GOODIES_LOCATION}/config_loader/config_loader.rb -o lib/config_loader.rb"
-run <<-EOF
-patch config/environment.rb <<EOP
+
+patch 'config/environment.rb', <<-EOP
 diff --git a/config/environment.rb b/config/environment.rb
 index 029a58b..6ecb031 100644
 --- a/config/environment.rb
@@ -34,14 +49,14 @@ index 029a58b..6ecb031 100644
  
 
 EOP
-EOF
 
-git :add => '.', :commit => 'Installed Config Loader'
+git :add => '.'
+git :commit => "-m 'Installed Config Loader'"
 
 # Authentication
 if yes?('Perform Authentication?')
   user_model = ask 'User Model'
   plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
   generate :authenticated, user_model, 'Session'
-  git :add => '.', :commit => 'Basic Authentication'
+  git :add => '.', :commit => "-m 'Basic Authentication'"
 end
